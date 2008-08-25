48a3790121a71ef135423c6827d181247bd4a70a
John Stumpo <stump@jstump.com>
2008-08-25 00:34:52 +0000

    fix detection of the most recent change
    git-svn-id: svn://svn.jstump.com/r/kexec/trunk@12 58bca3e3-592c-47f3-bada-4a0745530ac1

--- SvnRevision.py
+++ SvnRevision.py
@@ -33,8 +33,8 @@ for arg in sys.argv[1:]:
   highestrev = 0
   for dep in deps.split(','):
     try:
-      rev = int(os.popen("svn info '%s' | grep ^Revision | egrep -o '[0-9]+'" %
-        dep.replace("'", "'\"'\"'"), 'r').read())
+      rev = int(os.popen(("svn info '%s' | grep '^Last Changed Rev' | " +
+        "egrep -o '[0-9]+'" % dep.replace("'", "'\"'\"'")), 'r').read())
     except ValueError:
       rev = 0
     if rev > highestrev:
